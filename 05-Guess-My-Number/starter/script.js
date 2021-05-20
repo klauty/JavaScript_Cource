b082e9d1fae7af0ba78bd28b4a2ee40da2d1a888
klauty <kaue.developer@gmail.com>
2021-05-20 00:32:54 -0300

    primeiro commit

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,9 @@
+'use strict';
+/*lendo texto do componente
+document.querySelector('.message').textContent = 'lendo componente';
+
+//lendo imput
+document.querySelector('.guess').value = 23;
+*/
+
+document.querySelector('.check').addEventListener('click');
